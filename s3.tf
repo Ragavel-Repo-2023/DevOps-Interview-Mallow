ea295d3b500fd87a67b2f214f3619b4eded63b6f
Ragavel-Repo-2023 <129276576+Ragavel-Repo-2023@users.noreply.github.com>
2025-07-15 10:55:07 +0100

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,10 +1,16 @@
+locals {
+  sanitized_cluster_name = lower(replace(var.cluster_name, "_", "-"))
+}
+
 resource "aws_s3_bucket" "app_bucket" {
-  bucket = "${var.cluster_name}-static-bucket"
+  bucket = "${local.sanitized_cluster_name}-static-bucket"
   force_destroy = true
+
   tags = {
-    Name = "${var.cluster_name}-static-bucket"
+    Name = "${local.sanitized_cluster_name}-static-bucket"
   }
 }
+
 
 resource "aws_s3_bucket_public_access_block" "app_bucket_block" {
   bucket = aws_s3_bucket.app_bucket.id
