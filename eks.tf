5a248df5c06b90fdc4aee939a57200f3441b6dcc
Ragavel-Repo-2023 <129276576+Ragavel-Repo-2023@users.noreply.github.com>
2025-07-16 07:50:08 +0100

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -14,14 +14,4 @@ module "eks" {
   subnet_ids      = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
   vpc_id          = aws_vpc.main.id
   cluster_security_group_id = aws_security_group.eks_cluster_sg.id
-
-  node_groups = {
-    rails_nodes = {
-      desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
-      instance_type    = "t3.medium"
-      subnet_ids       = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
-    }
-  }
 }
